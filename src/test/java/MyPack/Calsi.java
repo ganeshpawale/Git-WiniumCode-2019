ec7cb2fed59530c36c9d62c979ecd7f72693f8b9
ganeshpawale <ganeshpawale47@gmail.com>
2019-06-16 11:52:21 +0530

    Eight commit

--- src/test/java/MyPack/Calsi.java
+++ src/test/java/MyPack/Calsi.java
@@ -14,7 +14,7 @@ public class Calsi {
       DesktopOptions option = new DesktopOptions();
       option.setApplicationPath("C:\\Windows\\System32\\calc.exe");//"C:\Windows\System32\notepad.exe"
       WiniumDriver driver =new WiniumDriver(new URL("http://localhost:9999"), option);
-      Thread.sleep(5000); 
+      Thread.sleep(5000);//Given 5milisecond thread 
       driver.findElement(By.name("7")).click(); //Find seven digit button
       driver.findElement(By.name("Add")).click(); //Find add digit button
       driver.findElement(By.name("5")).click(); //Find Five digit button
