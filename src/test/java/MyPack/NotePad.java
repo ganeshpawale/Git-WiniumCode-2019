1fe7cb5f3307346ce5afa5b22eb96bf0032cab50
ganeshpawale <ganeshpawale47@gmail.com>
2019-06-16 11:25:44 +0530

    Seven commit

--- src/test/java/MyPack/NotePad.java
+++ src/test/java/MyPack/NotePad.java
@@ -12,7 +12,7 @@ public class NotePad {
 	public static void main(String[] args) throws InterruptedException, MalformedURLException {
 		
 		DesktopOptions option =new DesktopOptions();//DesktopOtions is class
-		option.setApplicationPath("C:\\Windows\\System32\\notepad.exe");
+		option.setApplicationPath("C:\\Windows\\System32\\notepad.exe");//Give notepad application exe file path
 		WiniumDriver driver =new WiniumDriver(new URL("http://localhost:9999"),option);
 		Thread.sleep(5000);
 		driver.findElement(By.id("15")).sendKeys("my name is Jai Ganesh");//Find textbox for put value
